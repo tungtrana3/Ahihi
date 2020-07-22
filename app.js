8bf0527a93cedcf97c74e52fc4a9553892b897d5
tungtran <tungtrana3@gmail.com>
2020-07-22 14:44:03 +0700

    cm

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+app.use('/img', express.static('public/img'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use('/css', express.static('public/css'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
