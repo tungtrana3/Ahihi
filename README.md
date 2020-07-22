407c8e0066a64ea6edd0665792fc020d1d7a57c7
tungtran <tungtrana3@gmail.com>
2020-07-22 21:56:26 +0700

    cm

---
+++ README.md
@@ -0,0 +1,1 @@
+# ServerShopping
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var Router = require('./routes/routeServer');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', Router);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
