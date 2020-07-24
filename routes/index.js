bf9422c68ebcc7364f7928f7e5e6bdf8d84acc99
tungtran <tungtrana3@gmail.com>
2020-07-24 14:00:35 +0700

    del

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var AccountController = require('../controller/AccountController');
+// var AccountController = require('../controller/AccountController');
 // var proType = require('./routeProductType');
 // var routeAccount = require('./routeAccount');
 // var routeProduct = require('./routeProduct');
@@ -9,8 +9,8 @@ router.get('/', function (req, res, next) {
   // req.session.isLogin = false
   return res.render('login/login');
 });
-router.route('/login')
-  .post(AccountController.login)
+// router.route('/login')
+//   .post(AccountController.login)
 router.get('/home', function (req, res) {
   return res.render('pages/index');
   // if (req.session.isLogin) {
